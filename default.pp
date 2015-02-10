275ff5d2508b09dc605eaaebf652b345158f7673
Adrian Webb <adrian.webb@coraltech.net>
2015-02-10 09:48:39 -0500

    Removing the default php modules.

--- default.pp
+++ default.pp
@@ -1,13 +1,5 @@
 
 class coralnexus::web::default {
   $web_home = '/var/www'
-
-  $apache_icinga2web_classes = [
-    "php::mod::curl",
-    "php::mod::gd",
-    "php::mod::xmlrpc",
-    "php::mod::uploadprogress",
-    "php::mod::xdebug"
-  ]
 }
 
